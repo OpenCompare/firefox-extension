558fc853f7a825ceca7f84360683144bf153e2dd
Guillaume Bécan <guillaume.becan@irisa.fr>
2015-09-17 14:15:53 +0200

    show panel when clicking on extension

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-var data = require("sdk/self").data;
-var self = require("sdk/self");
+(function() {
+    "use strict";
 
+    var data = require("sdk/self").data;
+    var panels = require("sdk/panel");
 
-require("sdk/ui/button/action").ActionButton({
-  id: "opencompare-extension",
-  label: "OpenCompare extension",
-  icon: "./icon.png",
-  onClick: function() {
-    worker = require("sdk/tabs").activeTab.attach({
-      contentScriptFile: [data.url('findTables.js')]
+    var button = require("sdk/ui/button/toggle").ToggleButton({
+        id: "opencompare-extension",
+        label: "OpenCompare extension",
+        icon: "./icon.png",
+        onChange: handleChange
     });
-  }
-});
+
+    var panel = panels.Panel({
+        contentURL: data.url("panel.html"),
+        onHide: handleHide
+    });
+
+    function handleChange(state) {
+        if (state.checked) {
+            panel.show({
+                position: button
+            });
+        }
+
+        require("sdk/tabs").activeTab.attach({
+            contentScriptFile: [data.url('findTables.js')]
+        });
+    }
+
+    function handleHide() {
+        button.state('window', {checked: false});
+    }
+})();
